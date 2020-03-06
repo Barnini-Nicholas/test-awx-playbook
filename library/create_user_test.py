60dc5563ed670978819475f725893def73342e95
Barnini Nicholas <nicholas.barnini@gmail.com>
2020-03-06 12:55:21 +0100

    test1

--- library/create_user_test.py
+++ library/create_user_test.py
@@ -4,14 +4,12 @@
 from ansible.module_utils.basic import *
 
 def main():
-	module = AnsibleModule(argument_spec={
-        var1    = dict(required=True, type='str'),
-        var2    = dict(required=True, type='str')
-    })
-	response = {"hello": "world"}
-	print("TEST LECTURE SCRIPT")
-	module.exit_json(changed=False, meta=response)
+    fields = {"yourName": {"required": True, "type": "str"}} 
+    module = AnsibleModule(argument_spec=fields)
+    response = {"hello": "world"}
+    print("TEST LECTURE SCRIPT")
+    module.exit_json(changed=False, meta=response)
 
 
 if __name__ == '__main__':
-	main()
+    main()
