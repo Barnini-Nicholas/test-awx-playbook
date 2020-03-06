8dbe89ee4ca5f474611c75640117eb0d8563d825
Barnini Nicholas <Barnini-Nicholas@users.noreply.github.com>
2020-03-06 11:45:45 +0100

    add python ansible module support

--- library/create_user_test.py
+++ library/create_user_test.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-print("TEST LECTURE SCRIPT")
+
+from ansible.module_utils.basic import *
+
+def main():
+
+	module = AnsibleModule(argument_spec={})
+	response = {"hello": "world"}
+  print("TEST LECTURE SCRIPT")
+	module.exit_json(changed=False, meta=response)
+
+
+if __name__ == '__main__':
+    main()
