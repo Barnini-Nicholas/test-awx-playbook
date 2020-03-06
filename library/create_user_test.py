604b0ed9d7615b1c5a9dad2e5d4deb8d01ad58f6
Barnini Nicholas <Barnini-Nicholas@users.noreply.github.com>
2020-03-06 11:47:53 +0100

    fix indent

--- library/create_user_test.py
+++ library/create_user_test.py
@@ -4,12 +4,11 @@
 from ansible.module_utils.basic import *
 
 def main():
-
 	module = AnsibleModule(argument_spec={})
 	response = {"hello": "world"}
-  print("TEST LECTURE SCRIPT")
+	print("TEST LECTURE SCRIPT")
 	module.exit_json(changed=False, meta=response)
 
 
 if __name__ == '__main__':
-    main()
+	main()
