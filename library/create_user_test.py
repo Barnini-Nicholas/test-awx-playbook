447243f34667509014eab3404c3deac818e97010
Barnini Nicholas <nicholas.barnini@gmail.com>
2020-03-06 12:18:00 +0100

    add var in py

--- library/create_user_test.py
+++ library/create_user_test.py
@@ -4,7 +4,10 @@
 from ansible.module_utils.basic import *
 
 def main():
-	module = AnsibleModule(argument_spec={})
+	module = AnsibleModule(argument_spec={
+        var1    = dict(required=True),
+        var2    = dict(required=True, type='str')
+    })
 	response = {"hello": "world"}
 	print("TEST LECTURE SCRIPT")
 	module.exit_json(changed=False, meta=response)
