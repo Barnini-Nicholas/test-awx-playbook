115ccea524ef11fb0daa58e58888ec93b8ca87a8
Barnini Nicholas <nicholas.barnini@gmail.com>
2020-03-06 14:37:33 +0100

    add vaar

--- library/create_user_test.py
+++ library/create_user_test.py
@@ -6,7 +6,9 @@ from ansible.module_utils.basic import *
 def main():
     fields = {"var1": {"required": True, "type": "str"}, "var2": {"required": True, "type": "str"}} 
     module = AnsibleModule(argument_spec=fields)
-    response = {"hello": "world"}
+    var1= module.params['var1']
+    var2= module.params['var2']
+    response = {var1: var2}
     print("TEST LECTURE SCRIPT")
     module.exit_json(changed=False, meta=response)
 
